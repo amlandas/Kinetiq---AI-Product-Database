5d062a2777bafba67fff413cd28fe89b54b10593
Amlan Das <dasamlan1984@gmail.com>
2026-01-12 18:44:56 -0800

    Add Google tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,18 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
+            <head>
+                <script async src="https://www.googletagmanager.com/gtag/js?id=G-WNLJMWSG9W" />
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-WNLJMWSG9W');`,
+                    }}
+                />
+            </head>
             <body className={inter.className}>{children}</body>
         </html>
     );
